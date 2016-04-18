37776875aac0f8305f67de0b20450a232522c96b
Michael Sjöström <micke_moj@hotmail.com>
2016-04-18 14:06:14 +0200

    added methods in Board.java

--- Lab1/src/Board.java
+++ Lab1/src/Board.java
@@ -11,13 +11,44 @@ public class Board {
 	public Iterable<Board> neighbors()     // all neighboring boards
 	public String toString()               // string representation of the board (in the output format specified below)*/
 	
-	private int[][] blocks;
+	public int[][] blocks;
+	private int size;
 	
 	
 	public Board(int[][] blocks){
 		this.blocks = blocks;
+		size = blocks[0].length;
 	}
 	
+	public int hamming(){
+		return 0;
+	}
 	
-
+	public boolean isGoal(){
+		return false;
+	}
+	
+	public boolean isSolvable(){
+		return false;
+	}
+	
+	public boolean equals(Object y){
+		return false;
+	}
+	
+	public Iterable<Board> neighbors(){
+		return null;
+	}
+	
+	public int getBoardSize(){
+		return size;
+	}
+	
+	public String toString(){
+		
+		String daBoard = "";
+		
+		
+		return null;
+	}
 }
