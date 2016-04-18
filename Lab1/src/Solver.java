4cad2970f46aa134c5dc0eb4ccf8f1f2d819d3bc
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-04-18 13:52:02 +0200

    added solver memberfunctions

--- Lab1/src/Solver.java
+++ Lab1/src/Solver.java
@@ -1,4 +1,27 @@
+import java.util.Arrays;
 
 public class Solver {
-	public Solver(){};
+	
+	String[] foo = {"hej", "hej2"};
+	
+	public Solver(){}
+	
+	public Solver(Board init){}
+	
+	public int moves(){
+		return 1;
+	}
+	
+	public Iterable<String> neighbors(){
+		return Arrays.asList(foo);
+	}
+	
+	public static void main(String[] args){ // some code here
+		System.out.println("running");
+	}
+	
+	
+	
+	
+	
 }
