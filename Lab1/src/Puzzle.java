f23f6f41ed315c2257fbcbee7ee8c650b283e4d8
Michael Sjöström <micke_moj@hotmail.com>
2016-04-18 17:35:02 +0200

    moved main

--- Lab1/src/Puzzle.java
+++ Lab1/src/Puzzle.java
@@ -28,5 +28,62 @@ public class Puzzle {
 		        heuristic += getManhattanDistance(i, array[i]);
 		    }*/
 		    return heuristic;
-		  }
+	  }
+	  
+	  public static void main(String[] args){ // some code here
+			System.out.println("running");
+			
+			int[] blocks = new int[9];
+			
+			//Hårdkodat for now..
+
+			blocks[0] = 0;
+			blocks[1] = 1;
+			blocks[2] = 3;
+			blocks[3] = 4;
+			blocks[4] = 2;
+			blocks[5] = 5;
+			blocks[6] = 7;
+			blocks[7] = 8;
+			blocks[8] = 6;
+			
+			int[] blocks2 = new int[9];
+			
+			//Hårdkodat for now..
+
+			blocks2[0] = 1;
+			blocks2[1] = 2;
+			blocks2[2] = 3;
+			blocks2[3] = 4;
+			blocks2[4] = 5;
+			blocks2[5] = 6;
+			blocks2[6] = 7;
+			blocks2[7] = 8;
+			blocks2[8] = 0;
+			
+			
+			
+			Board initial = new Board(blocks);
+			Board initial2 = new Board(blocks2);
+			
+			System.out.println(initial.toString());
+			System.out.println(initial.isGoal());
+			
+			System.out.println(initial2.toString());
+			System.out.println(initial2.isGoal());
+			
+			//initial.equals(initial);
+			
+			PriorityQueue<Board> pq = new PriorityQueue<Board>();
+			
+			System.out.println("hamming result: " +  initial.hamming());
+			
+			pq.add(initial);
+			
+			while(!initial.isGoal()){
+				
+				break;
+			}
+				
+		}
 }
