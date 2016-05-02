2d83acc82b5f0f2d53578155ecc3eec48d9e8b43
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-05-02 16:47:51 +0200

    override equals

--- Lab2/src/Clause.java
+++ Lab2/src/Clause.java
@@ -15,5 +15,21 @@ public class Clause {
 	public void setLiterals(List<String> literals){
 		this.literals = literals;
 	}
+	
+	@Override
+	public boolean equals(Object c){
+		
+		if(c instanceof Clause){
+			Clause test = (Clause)c;
+			
+			for(int i = 0, len = test.literals.size(); i < len; ++i){
+				if(test.literals.get(i) != this.literals.get(i))
+					return false;
+			}
+			return true;
+			
+		}
+		return false;
+	}
 
 }
