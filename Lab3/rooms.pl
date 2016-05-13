4a823da936047af1e6ded4463803ff5db3378c6f
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-05-13 11:04:23 +0200

    added roomconnections

--- Lab3/rooms.pl
+++ Lab3/rooms.pl
@@ -2,43 +2,19 @@
 
 %Shakeys actions
 act( go(X, Y),
-     [sAt(X)],               % preconditions
-     [sAt(X)],                % delete
-     [sAt(Y)]                % add
+     [sAt(X), in(s, room(X)), diff(X, Y)],               % preconditions
+     [sAt(X), in(s, room(X))],                % delete
+     [sAt(Y), in(s, room(Y))]                % add
      ).
-
-%Fråga va fan detta är
-%act( push(b, X, Y),
-
-act( climbUp(b),
-     [floor],
-     [floor],
-     [on]
-).
-
-act( climbDown(b),
-     [on],
-     [on],
-     [floor]).
-
-act( turnon(sw),
-    [on(s, box), lightoff(room)],    %preconditions
-    [lightoff(room)],                %delete
-    [lighton(room)]
-).
-
-act( turnoff(sw),
-    [on(s, box), lighton(room)],    %preconditions
-    [lighton(room)],                %delete
-    [lightoff(room)]
-).
 
 % States
 % Shakey should be in room1
 % Switch of light in room1
 % Box2 should be in room2
 
-goal_state( [in(s, room1),  in(box2, room2), lightoff(room2)] ).
+%goal_state( [in(s, room1),  in(box2, room2), lightoff(room2)] ).
+
+goal_state( [in(s, room1) ] ).
 
 initial_state( [
         in(s, room3),
@@ -50,6 +26,11 @@ initial_state( [
         room(room2),
         room(room3),
         room(room4),
+        room(korr),
+        connected(room1, korr),
+        connected(room2, korr),
+        connected(room3, korr),
+        connected(room4, korr),
         box(box1),
         box(box2),
         box(box3),
@@ -58,6 +39,7 @@ initial_state( [
         lightoff(room2),
         lighton(room1),
         lighton(room4),
+        lighton(korr),
         diff(box1, box2),
         diff(box2, box3),
         diff(box3, box4),
@@ -69,6 +51,14 @@ initial_state( [
         diff(room3, room4),
         diff(room2, room1),
         diff(room3, room2),
-        diff(room4, room3)
+        diff(room4, room3),
+        diff(room1, korr),
+        diff(room2, korr),
+        diff(room3, korr),
+        diff(room4, korr), 
+        diff(korr, room1),
+        diff(korr, room2),
+        diff(korr, room3),
+        diff(korr, room4) 
     ]). 
 
