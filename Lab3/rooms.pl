31f2a2a62ff420535184590a5f1494996aa5eeea
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-05-16 13:20:23 +0200

    pushing working stuff

--- Lab3/rooms.pl
+++ Lab3/rooms.pl
@@ -3,12 +3,12 @@
 %Shakeys actions
 act( go(X, Y),
 
-     [in(s, room), in(X, room), in(Y, room), sAt(X), diff(X, Y)],               % preconditions
-     [sAt(X)],                % delete
-     [sAt(Y)]                % add
-     ).
+    [sAt(X), in(s, Room)],               % preconditions
+    [sAt(X)],                            % delete
+    [in(s, Room1)]                       % add
 
-%Fråga va fan detta är
+).
+
 %act( push(b, X, Y),
 
 %act( climbUp(b),
@@ -41,38 +41,26 @@ act( go(X, Y),
 
 %goal_state( [in(s, room1),  in(box2, room2), lightoff(room2)] ).
 
-goal_state( [in(s, room1) ] ).
+goal_state( [in(s, Room1) ] ).
 
+%goal_state( [sAt(X)] ).
+    
 initial_state( [
         in(s, room3),
-        in(box1, room1),
-        in(box2, room1),
-        in(box3, room1),
-        in(box4, room1),
+        %in(box1, room1),
+        %in(box2, room1),
+        %in(box3, room1),
+        %in(box4, room1),
         room(room1),
         room(room2),
         room(room3),
         room(room4),
         room(korr),
+        handempty,
         connected(room1, korr),
         connected(room2, korr),
         connected(room3, korr),
         connected(room4, korr),
-        box(box1),
-        box(box2),
-        box(box3),
-        box(box4),
-        lightoff(room3),
-        lightoff(room2),
-        lighton(room1),
-        lighton(room4),
-        lighton(korr),
-        diff(box1, box2),
-        diff(box2, box3),
-        diff(box3, box4),
-        diff(box2, box1),
-        diff(box3, box2),
-        diff(box4, box3),
         diff(room1, room2),
         diff(room2, room3),
         diff(room3, room4),
@@ -88,4 +76,20 @@ initial_state( [
         diff(korr, room3),
         diff(korr, room4) 
     ]). 
+
+        %box(box1),
+        %box(box2),
+        %box(box3),
+        %box(box4),
+        %lightoff(room3),
+        %lightoff(room2),
+        %lighton(room1),
+        %lighton(room4),
+        %lighton(korr),
+        %diff(box1, box2),
+        %diff(box2, box3),
+        %diff(box3, box4),
+        %diff(box2, box1),
+        %diff(box3, box2),
+        %diff(box4, box3),
 
