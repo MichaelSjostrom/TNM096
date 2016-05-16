d41e3cd28b5fd06739220684445ac4155840df1d
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-05-16 15:49:13 +0200

    only moving boxes and turning on/off lights

--- Lab3/rooms.pl
+++ Lab3/rooms.pl
@@ -2,14 +2,14 @@
 
 %Shakeys actions
 act( goBetweenRooms(Room, Room2),
-    [in(s, Room), connected(Room, Room2)],
+    [in(s, Room), connected(Room, Room2), diff(Room, Room2)],
     [in(s, Room)],
     [in(s, Room2)]
 ).
 
 
 act( go(X, Y), 
-    [in(s, Room), in(X, Room), in(Y, Room), in(s, X)],
+    [in(s, Room), in(X, Room), in(Y, Room), in(s, X), diff(X, Y)],
     [in(s, X)],
     [in(s, Y)]
 ).
@@ -41,13 +41,13 @@ act( climbDown(Box),
 %).
 
 act( pushBoxBetweenRooms(Box, Room, Room2),
-    [in(s, Room), on(s, floor), in(Box, Room), connected(Room, Room2)],
+    [box(Box), in(s, Room), on(s, floor), in(Box, Room), connected(Room, Room2)],
     [in(s, Room), in(Box, Room)],
     [in(s, Room2), in(Box, Room2)]
 ).
 
 act( pushBox(Box, X, Y),
-    [in(s, X), in(Box, X), on(s, floor), handempty],
+    [box(Box), in(s, X), in(Box, X), on(s, floor), handempty],
     [in(s, X), in(Box, X), handempty],
     [in(s, Y), in(Box, Y), handempty]
 ).
@@ -57,23 +57,23 @@ act( pushBox(Box, X, Y),
 % Switch of light in room1
 % Box2 should be in room2
 
-goal_state( [in(s, room1), lightoff(sw1)] ).
+%goal_state( [in(s, room1), lightoff(sw1)] ).
 
 %goal_state( [in(s, room1),  in(box2, room2), lightoff(room2)] ).
 
 %goal_state( [in(s, room1), in(box1, sw1)]).
 
 %Go to room1
-%goal_state( [in(s, room1) ] ).
+%goal_state( [ in(s, room1) ] ).
 
 %Climb on Box2
-%goal_state( [on(s, Box2)] ).
+%goal_state( [on(s, box2)] ).
 
 %Turn off light in room1, without standing on a box atm
 %goal_state( [in(s, room1), lightoff(sw1)] ).
 
 %Move box to room2
-%goal_state( [ in(box2, room2 )] ).
+goal_state( [ in(box2, room2 )] ).
 
 initial_state( [
         in(s, room3),
@@ -117,9 +117,7 @@ initial_state( [
         box(box1),
         box(box2),
         diff(box1, box2),
-        diff(box2, box1),
-        in(box1, room1),
-        in(box2, room1),        
+        diff(box2, box1),     
         diff(room1, room2),
         diff(room2, room3),
         diff(room3, room4),
