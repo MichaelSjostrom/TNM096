506317fb396169630c7734d0c2a6e29c44771159
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-05-16 14:43:02 +0200

    movebox and lighton

--- Lab3/rooms.pl
+++ Lab3/rooms.pl
@@ -2,37 +2,28 @@
 
 %Shakeys actions
 act( goBetweenRooms(Room, Room2),
-
     [in(s, Room), connected(Room, Room2)],
     [in(s, Room)],
     [in(s, Room2)]
-
 ).
 
-%act( push(b, X, Y),
+act( turnLightOff(SW),
+    [in(s, Room), lighton(SW)],
+    [lighton(SW)],
+    [lightoff(SW)]
+).
 
-%act( climbUp(b),
-%     [floor],
-%     [floor],
-%     [on]
+%act( turnLightOn(SW),
+%    [in(s, Room), lightoff(SW)],
+%    [lightoff(SW)],
+%    [lighton(SW)]
 %).
 
-%act( climbDown(b),
-%     [on],
-%     [on],
-%     [floor]).
-
-%act( turnon(sw),
-%    [on(s, box), lightoff(room)],    %preconditions
-%    [lightoff(room)],                %delete
-%    [lighton(room)]
-%).
-
-%act( turnoff(sw),
-%    [on(s, box), lighton(room)],    %preconditions
-%    [lighton(room)],                %delete
-%    [lightoff(room)]
-%).
+act( pushBox(Box, Room, Room2),
+    [in(s, Room), in(Box, Room), connected(Room, Room2)],
+    [in(s, Room), in(Box, Room)],
+    [in(s, Room2), in(Box, Room2)]
+).
 
 % States
 % Shakey should be in room1
@@ -41,14 +32,19 @@ act( goBetweenRooms(Room, Room2),
 
 %goal_state( [in(s, room1),  in(box2, room2), lightoff(room2)] ).
 
-goal_state( [in(s, room1) ] ).
+%Go to room1
+%goal_state( [in(s, room1) ] ).
 
-%goal_state( [sAt(X)] ).
+%Turn off light in room1, without standing on a box atm
+%goal_state( [in(s, room1), lightoff(sw1)] ).
     
+goal_state( [ in(box2, room2 )] ).
+
 initial_state( [
         s,
-        sAt(init),
         in(s, room3),
+        in(box1, room1),
+        in(box2, room1),
         room(room1),
         room(room2),
         room(room3),
@@ -62,6 +58,10 @@ initial_state( [
         connected(room3, korr),
         connected(korr, room3),
         connected(room4, korr),
+        box(box1),
+        box(box2),
+        diff(box1, box2),
+        diff(box2, box1),
         diff(room1, room2),
         diff(room2, room3),
         diff(room3, room4),
@@ -75,22 +75,16 @@ initial_state( [
         diff(korr, room1),
         diff(korr, room2),
         diff(korr, room3),
-        diff(korr, room4) 
+        diff(korr, room4),
+        lighton(sw1),
+        lightoff(sw2),
+        lightoff(sw3),
+        lightoff(sw4)
     ]). 
 
-        %box(box1),
-        %box(box2),
-        %box(box3),
-        %box(box4),
         %lightoff(room3),
         %lightoff(room2),
         %lighton(room1),
         %lighton(room4),
         %lighton(korr),
-        %diff(box1, box2),
-        %diff(box2, box3),
-        %diff(box3, box4),
-        %diff(box2, box1),
-        %diff(box3, box2),
-        %diff(box4, box3),
-
+        
